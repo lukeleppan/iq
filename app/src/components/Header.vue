8bb997175c62d6342f6aa6a8f34022ff9db08ac7
Luke Leppan <lukelepp@gmail.com>
2021-04-03 11:18:52 +0200

    vetur

---
+++ app/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style></style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header title="iQ" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Header from "@/components/Header";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { Header },
+};
 </script>
