74db90a12a134cf42d0195fcfe7706a04c39df2d
Luke Leppan <lukelepp@gmail.com>
2021-04-13 23:24:35 +0200

    fix

--- app/src/main.js
+++ app/src/main.js
@@ -2,11 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import VueMeta from "vue-meta";
 import store from "./store";
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(VueMeta)
   .mount("#app");
